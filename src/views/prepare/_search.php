41388f5611d35e8c23b56663ff7c1fd512980301
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-09-18 17:32:26 +0200

    Changed client_in input to textarea

--- src/views/prepare/_search.php
+++ src/views/prepare/_search.php
@@ -35,8 +35,8 @@ use hiqdev\combo\StaticCombo;
 </div>
 
 <div class="col-md-4 col-sm-6 col-xs-12">
-    <?= $search->field('client_in')->widget(ClientCombo::class, [
-        'multiple' => true,
+    <?= $search->field('client_in')->textarea([
+        'placeholder' => Yii::t('hipanel:mailing', 'Client logins list (comma-, or space-separated)')
     ]) ?>
 </div>
 
